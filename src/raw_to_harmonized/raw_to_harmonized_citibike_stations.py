f03889f1dae7f92579e92f05e89bea12ffa645c9
Maria Skripova <maria@skripova.com>
2021-08-17 11:08:51 +0200

    Adding the updated files

--- src/raw_to_harmonized/raw_to_harmonized_citibike_stations.py
+++ src/raw_to_harmonized/raw_to_harmonized_citibike_stations.py
@@ -4,7 +4,7 @@ def read_json():
     with open("data/raw/citibike_data_full.json","r") as jsonfile:
         line = jsonfile.read()
         dictionary = json.loads(line)
-        return (dictionary)
+        return dictionary
 
 def get_sorted_keys(dictionary):
     list_of_keys=[]
@@ -12,18 +12,23 @@ def get_sorted_keys(dictionary):
         for keys in i.keys():
                 list_of_keys.append(keys)
     sorted_list_of_keys = sorted(set(list_of_keys))
-    sorted_list_of_keys = (sorted_list_of_keys[0],sorted_list_of_keys[1],sorted_list_of_keys[2],sorted_list_of_keys[3],
-    sorted_list_of_keys[8],sorted_list_of_keys[9],sorted_list_of_keys[10],sorted_list_of_keys[11])
+    sorted_list_of_keys = (sorted_list_of_keys[8],sorted_list_of_keys[9],sorted_list_of_keys[10],sorted_list_of_keys[11])
     return sorted_list_of_keys
 
 def get_list_of_rows_from_dict(dictionary, sorted_list_of_keys):
-
     list_of_rows = []
     values = []
     for i in dictionary:
         values = []
         for key in sorted_list_of_keys:
             values.append(i[key])
+        
+        count = 0
+        for k in values[:2]:
+            new = k[:7]
+            values[count] = new
+            count = count + 1
+
         if values in list_of_rows:
             pass
         else:
@@ -32,11 +37,10 @@ def get_list_of_rows_from_dict(dictionary, sorted_list_of_keys):
 
 def write_to_file(data, filepath):
     with open(filepath, "w") as open_file:
-        json.dump(data, open_file)
+        json.dump(data, open_file, indent= 2)
 
 dicto = read_json()
 list_of_keys = get_sorted_keys(dicto)
 list_of_row = get_list_of_rows_from_dict(dicto,list_of_keys)
 write_to_file(list_of_keys,"data/raw_to_harmonized/columns.json")
 write_to_file(list_of_row,"data/raw_to_harmonized/list_of_tuples.json")
-
