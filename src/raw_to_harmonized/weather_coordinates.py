7e75672537587c50bacc917b62481370cc371457
flytoeren <flytoeren@hotmail.com>
2021-08-18 14:15:08 +0200

    changed name of stations harmonized data

--- src/raw_to_harmonized/weather_coordinates.py
+++ src/raw_to_harmonized/weather_coordinates.py
@@ -2,8 +2,8 @@ import json
 
 def open_weather_stations(filepath):
     with open(filepath, "r") as opened_file:
-        our_files = json.load(opened_file)
-        return our_files
+        station_data = json.load(opened_file)
+        return station_data
 
 def write_stations_rows(stations_dict, filepath):
     with open(filepath, "w") as opened_file:
@@ -16,12 +16,12 @@ def write_stations_columns(columns_list, filepath):
 
 if __name__ == "__main__":
     file_path = "data/raw/weather_coordinates.json"
-    our_files = open_weather_stations(file_path)
+    station_data = open_weather_stations(file_path)
 
     file_path = "data/harmonized/weather_coordinates_rows.json"
-    write_stations_rows(our_files, file_path)
+    write_stations_rows(station_data, file_path)
 
     columns_list = ["stations_id", "location", "latitude", "longitude"]
 
-    file_path = "data/harmonized/weather_coordinates_columns.json"
+    file_path = "data/harmonized/weather_station.json"
     write_stations_columns(columns_list, file_path)
