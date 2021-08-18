3685d15673c617a0e440fc4aaa7be4cf5c7ac75a
Rasmus <rasmusandreasson@outlook.com>
2021-08-18 08:24:58 +0200

    changes

--- src/raw_to_harmonized/raw_to_harmonized_citibike_stations.py
+++ src/raw_to_harmonized/raw_to_harmonized_citibike_stations.py
@@ -1,5 +1,4 @@
 import json
-import time
 
 def read_json():
     with open("data/raw/citibike_data_full.json","r") as jsonfile:
@@ -13,7 +12,7 @@ def get_sorted_keys_start(dictionary):
         for keys in i.keys():
                 list_of_keys.append(keys)
     sorted_list_of_keys = sorted(set(list_of_keys))
-    print(sorted_list_of_keys)
+    
     sorted_list_of_keys_start = (sorted_list_of_keys[8],sorted_list_of_keys[9],sorted_list_of_keys[10],sorted_list_of_keys[11])
     return sorted_list_of_keys_start
 
@@ -23,7 +22,7 @@ def get_sorted_keys_end(dictionary):
         for keys in i.keys():
                 list_of_keys.append(keys)
     sorted_list_of_keys = sorted(set(list_of_keys))
-    print(sorted_list_of_keys)
+    
     sorted_list_of_keys_end = (sorted_list_of_keys[0],sorted_list_of_keys[1],sorted_list_of_keys[2],sorted_list_of_keys[3])
     return sorted_list_of_keys_end
 
@@ -34,12 +33,6 @@ def get_list_of_rows_from_dict(dictionary, sorted_list_of_keys_start):
         values = []
         for key in sorted_list_of_keys_start:
             values.append(i[key])
-
-        count = 0 
-        for k in values[:2]:
-            new = k[:7]
-            values[count] = new
-            count = count + 1
 
         if values in list_of_rows:
             pass
@@ -53,23 +46,41 @@ def adding_missing_stations(dictionary,list_of_rows,list_of_keys_end):
         values = []
         for key in list_of_keys_end:
             values.append(i[key])
-    
-        if len(values) != 4:
-            pass
-        else:
-            count = 0 
-            for k in values[:2]:
-                new = k[:7]
-                values[count] = new
-                count = count + 1
             
-            for i in list_of_rows:
-                if values not in list_of_rows:
-                    list_of_rows.append(values)
-                else:
-                    pass
+        for i in list_of_rows:
+            if values not in list_of_rows:
+                list_of_rows.append(values)
+            else:
+                pass
 
     return list_of_rows
+
+def removing_missing_values(new_list_of_rows):
+    lista = []
+    for i in new_list_of_rows:
+        if "" in i:
+            pass
+        else:
+            lista.append(i)
+    return lista
+
+def removing_duplicates(list_of_rows):
+    lists_of_ids = []
+    new_list_of_rows = []
+    for lists in list_of_rows:
+        if lists[2] not in lists_of_ids:
+            lists_of_ids.append(lists[2])
+        else:
+            pass
+    
+    lists_of_ids=set(lists_of_ids)
+
+    for i in list_of_rows:
+        if i[2] in lists_of_ids:
+            new_list_of_rows.append(i)
+            lists_of_ids.remove(i[2])
+    
+    return new_list_of_rows                      
 
 def write_to_file(data, filepath):
     with open(filepath, "w") as open_file:
@@ -80,7 +91,9 @@ list_of_keys_start = get_sorted_keys_start(dicto)
 list_of_keys_end = get_sorted_keys_end(dicto)
 list_of_row = get_list_of_rows_from_dict(dicto,list_of_keys_start)
 new_list_of_rows = adding_missing_stations(dicto,list_of_row,list_of_keys_end)
+lastone = removing_missing_values(new_list_of_rows)
+last2=removing_duplicates(lastone)
 
 write_to_file(list_of_keys_start,"data/raw_to_harmonized/columns.json")
-write_to_file(new_list_of_rows,"data/raw_to_harmonized/list_of_tuples.json")
+write_to_file(last2,"data/raw_to_harmonized/list_of_tuples.json")
 
