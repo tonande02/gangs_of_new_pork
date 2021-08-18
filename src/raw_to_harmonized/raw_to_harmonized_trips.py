5ad11ebebf0e447d1e9b2aa9d06518ba9916e4d6
Maria Skripova <maria@skripova.com>
2021-08-18 09:21:18 +0200

    Adding script for trips and jsons for trips

--- src/raw_to_harmonized/raw_to_harmonized_trips.py
+++ src/raw_to_harmonized/raw_to_harmonized_trips.py
@@ -28,50 +28,7 @@ def get_list_of_rows_from_dict(dictionary, sorted_list_of_keys):
             pass
         else:
             list_of_rows.append(values)
-    print(list_of_rows)
     return list_of_rows
-
-def adding_missing_stations(dictionary,list_of_rows,list_of_keys_end):
-    values = []
-    for i in dictionary:
-        values = []
-        for key in list_of_keys_end:
-            values.append(i[key])
-            
-        for i in list_of_rows:
-            if values not in list_of_rows:
-                list_of_rows.append(values)
-            else:
-                pass
-
-    return list_of_rows
-
-def removing_missing_values(new_list_of_rows):
-    lista = []
-    for i in new_list_of_rows:
-        if "" in i:
-            pass
-        else:
-            lista.append(i)
-    return lista
-
-def removing_duplicates(list_of_rows):
-    lists_of_ids = []
-    new_list_of_rows = []
-    for lists in list_of_rows:
-        if lists[2] not in lists_of_ids:
-            lists_of_ids.append(lists[2])
-        else:
-            pass
-    
-    lists_of_ids=set(lists_of_ids)
-
-    for i in list_of_rows:
-        if i[2] in lists_of_ids:
-            new_list_of_rows.append(i)
-            lists_of_ids.remove(i[2])
-    
-    return new_list_of_rows                      
 
 def write_to_file(data, filepath):
     with open(filepath, "w") as open_file:
@@ -80,10 +37,7 @@ def write_to_file(data, filepath):
 dicto = read_json()
 list_of_keys = get_sorted_keys(dicto)
 list_of_row = get_list_of_rows_from_dict(dicto,list_of_keys)
-#new_list_of_rows = adding_missing_stations(dicto,list_of_row,list_of_keys_end)
-#lastone = removing_missing_values(new_list_of_rows)
-#last2=removing_duplicates(lastone)
 
-#write_to_file(list_of_keys_start,"data/raw_to_harmonized/columns.json")
-#write_to_file(last2,"data/raw_to_harmonized/list_of_tuples.json")
+write_to_file(list_of_keys,"data/raw_to_harmonized/columns_trips.json")
+write_to_file(list_of_row,"data/raw_to_harmonized/list_of_tuples_trips.json")
 
