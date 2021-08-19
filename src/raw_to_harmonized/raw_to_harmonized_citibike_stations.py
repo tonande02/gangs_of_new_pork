12f4296333dc139c0e6054c232be50a3c5683bb5
Rasmus <rasmusandreasson@outlook.com>
2021-08-19 08:27:45 +0200

    harmonozed

--- src/raw_to_harmonized/raw_to_harmonized_citibike_stations.py
+++ src/raw_to_harmonized/raw_to_harmonized_citibike_stations.py
@@ -3,28 +3,36 @@ import json
 def read_json():
     with open("data/raw/citibike_data_full.json","r") as jsonfile:
         line = jsonfile.read()
-        dictionary = json.loads(line)
-        return dictionary
+        list_of_dicts = json.loads(line)
+        return list_of_dicts
 
-def get_sorted_keys_start(dictionary):
-    list_of_keys=[]
-    for i in dictionary:
-        for keys in i.keys():
-                list_of_keys.append(keys)
-    sorted_list_of_keys = sorted(set(list_of_keys))
-    
+
+
+def get_sorted_keys_start(list_of_dicts):
+    list_of_keys_start=[]
+    for dict in list_of_dicts: #Going through each dict within the list of dicts
+        for keys in dict.keys(): 
+                list_of_keys_start.append(keys) #Appending each key that exist in the dicts into a list
+    sorted_list_of_keys = sorted(set(list_of_keys_start))
+    #Picking out all the unqiue keys(set) and sorting(sorted) them since they allways comes in diffrent orders.
+
     sorted_list_of_keys_start = (sorted_list_of_keys[8],sorted_list_of_keys[9],sorted_list_of_keys[10],sorted_list_of_keys[11])
-    return sorted_list_of_keys_start
+    return sorted_list_of_keys_start #returning desired keys in list
 
-def get_sorted_keys_end(dictionary):
-    list_of_keys=[]
-    for i in dictionary:
-        for keys in i.keys():
-                list_of_keys.append(keys)
-    sorted_list_of_keys = sorted(set(list_of_keys))
-    
+
+
+def get_sorted_keys_end(list_of_dicts):
+    list_of_keys_end=[]
+    for dict in list_of_dicts: #Going through each dict within the list of dicts
+        for keys in dict.keys():
+                list_of_keys_end.append(keys) #Appending each key that exist in the dicts into a list
+    sorted_list_of_keys = sorted(set(list_of_keys_end))
+     #Picking out all the unqiue keys(set) and sorting(sorted) them since they allways comes in diffrent orders.
+
     sorted_list_of_keys_end = (sorted_list_of_keys[0],sorted_list_of_keys[1],sorted_list_of_keys[2],sorted_list_of_keys[3])
-    return sorted_list_of_keys_end
+    return sorted_list_of_keys_end #returning desired keys in list
+
+
 
 def get_list_of_rows_from_dict(dictionary, sorted_list_of_keys_start):
     list_of_rows = []
@@ -39,6 +47,8 @@ def get_list_of_rows_from_dict(dictionary, sorted_list_of_keys_start):
         else:
             list_of_rows.append(values)
     return list_of_rows
+
+
 
 def adding_missing_stations(dictionary,list_of_rows,list_of_keys_end):
     values = []
@@ -55,6 +65,8 @@ def adding_missing_stations(dictionary,list_of_rows,list_of_keys_end):
 
     return list_of_rows
 
+
+
 def removing_missing_values(new_list_of_rows):
     lista = []
     for i in new_list_of_rows:
@@ -63,6 +75,8 @@ def removing_missing_values(new_list_of_rows):
         else:
             lista.append(i)
     return lista
+
+
 
 def removing_duplicates(list_of_rows):
     lists_of_ids = []
@@ -86,7 +100,7 @@ def write_to_file(data, filepath):
     with open(filepath, "w") as open_file:
         json.dump(data, open_file, indent= 2)
 
-dicto = read_json()
+dicto = read_json() #reading json_file
 list_of_keys_start = get_sorted_keys_start(dicto)
 list_of_keys_end = get_sorted_keys_end(dicto)
 list_of_row = get_list_of_rows_from_dict(dicto,list_of_keys_start)
@@ -94,6 +108,6 @@ new_list_of_rows = adding_missing_stations(dicto,list_of_row,list_of_keys_end)
 lastone = removing_missing_values(new_list_of_rows)
 last2=removing_duplicates(lastone)
 
-write_to_file(list_of_keys_start,"data/harmonized/bike_stations_columns.json")
-write_to_file(last2,"data/harmonized/bike_stations_rows.json")
+write_to_file(list_of_keys_start,"data/raw_to_harmonized/columns.json")
+write_to_file(last2,"data/raw_to_harmonized/list_of_tuples.json")
 
