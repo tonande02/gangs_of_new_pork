40b7a1f6b328079ecbdacd3c0eaca250d3248325
Maria Skripova <maria@skripova.com>
2021-08-24 14:26:23 +0200

    Adding updated script and raw files for bikes

--- src/source_to_raw/source_to_raw_citibike.py
+++ src/source_to_raw/source_to_raw_citibike.py
@@ -4,31 +4,38 @@ import urllib.parse
 from zipfile import ZipFile
 import csv
 import json
-
-def download_zip_file():
-    zipurl = "https://s3.amazonaws.com/tripdata/JC-202106-citibike-tripdata.csv.zip"
+def download_zip_file(lista): #Downloads zipfile, extracts the csv and saves into data/raw folder.
+    zipurl = lista
     with urlopen(zipurl) as zipresp:
         with ZipFile(BytesIO(zipresp.read())) as zfile:
             zfile.extractall('data/raw')
-    
-    zipc = urllib.parse.urlsplit(zipurl)
+    zipc = urllib.parse.urlsplit(zipurl) #Renames the csv-file
     zipc= zipc.path.split("/")[-1]
     zipc=(zipc[:-4])
     return(zipc)
-
-def from_csv_to_json(file_name):
+def merging_all_csv_files(list_of_csv):
+    new_file = "data/raw/citibike_data_full.csv"
+    for i in list_of_csv:
+        with open(f"data/raw/{i}","r") as csvf:
+            file = csv.reader(csvf)
+            with open(new_file,"a") as large_csv:
+                writer = csv.writer(large_csv)
+                for line in file:
+                    writer.writerow(line)
+def from_csv_to_json(csvf): #Converts csv into json format.
     jsonArray = []
-
-    with open(f"data/raw/{file_name}",encoding="utf-8") as file:
+    with open(f"{csvf}",encoding="utf-8") as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             jsonArray.append(row)
-
-        with open("data/raw/citibike_data_full.json","w",encoding="utf-8") as new_file:
+        with open("data/raw/citibike_data_full.json","a",encoding="utf-8") as new_file:
             jsonString = json.dumps(jsonArray, indent = 4)
             new_file.write(jsonString)
-
-file_name = download_zip_file()
-
-from_csv_to_json(file_name)
-
+if __name__ == "__main__":
+    lista = ["https://s3.amazonaws.com/tripdata/JC-202106-citibike-tripdata.csv.zip","https://s3.amazonaws.com/tripdata/JC-202107-citibike-tripdata.csv.zip"]
+    list_of_csv = []
+    for i in lista:
+        file_name = download_zip_file(i)
+        list_of_csv.append(file_name)
+    merging_all_csv_files(list_of_csv)
+    from_csv_to_json("data/raw/citibike_data_full.csv")
