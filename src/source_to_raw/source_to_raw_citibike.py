173fd0b3424752f353f8f484b23b2b4a1e7cedd0
flytoeren <flytoeren@hotmail.com>
2021-08-24 15:32:42 +0200

    cleaning up, rm older files

--- src/source_to_raw/source_to_raw_citibike.py
+++ src/source_to_raw/source_to_raw_citibike.py
@@ -4,6 +4,8 @@ import urllib.parse
 from zipfile import ZipFile
 import csv
 import json
+
+
 def download_zip_file(lista): #Downloads zipfile, extracts the csv and saves into data/raw folder.
     zipurl = lista
     with urlopen(zipurl) as zipresp:
@@ -13,6 +15,8 @@ def download_zip_file(lista): #Downloads zipfile, extracts the csv and saves int
     zipc= zipc.path.split("/")[-1]
     zipc=(zipc[:-4])
     return(zipc)
+
+
 def merging_all_csv_files(list_of_csv):
     new_file = "data/raw/citibike_data_full.csv"
     for i in list_of_csv:
@@ -22,6 +26,8 @@ def merging_all_csv_files(list_of_csv):
                 writer = csv.writer(large_csv)
                 for line in file:
                     writer.writerow(line)
+
+
 def from_csv_to_json(csvf): #Converts csv into json format.
     jsonArray = []
     with open(f"{csvf}",encoding="utf-8") as file:
@@ -31,6 +37,8 @@ def from_csv_to_json(csvf): #Converts csv into json format.
         with open("data/raw/citibike_data_full.json","a",encoding="utf-8") as new_file:
             jsonString = json.dumps(jsonArray, indent = 4)
             new_file.write(jsonString)
+
+
 if __name__ == "__main__":
     lista = ["https://s3.amazonaws.com/tripdata/JC-202106-citibike-tripdata.csv.zip","https://s3.amazonaws.com/tripdata/JC-202107-citibike-tripdata.csv.zip"]
     list_of_csv = []
