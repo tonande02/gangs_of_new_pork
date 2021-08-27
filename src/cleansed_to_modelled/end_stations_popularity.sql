fe4e02f06cb800c7f0796101fc7888a9dd367a3c
flytoeren <flytoeren@hotmail.com>
2021-08-27 11:09:40 +0200

    changes

--- src/cleansed_to_modelled/end_stations_popularity.sql
+++ src/cleansed_to_modelled/end_stations_popularity.sql
@@ -1,16 +1,16 @@
 -- shows the 5 most popular end stations, and 50 non-station drop of points
 
-SELECT * FROM (
+select * from (
 select count (cbd.end_station_id) as total_rides, cbd.end_station_id, cbs.station_name from cleansed.bike_data as cbd
-join cleansed.bike_station as cbs ON cbd.end_station_id = cbs.station_id
+join cleansed.bike_station as cbs on cbd.end_station_id = cbs.station_id
 group by cbd.end_station_id, cbs.station_name
 order by count (cbd.end_station_id) desc
-limit 5) AS most_popular
-UNION
-SELECT * FROM (
+limit 5) as most_popular
+union
+select * from (
 select count (cbd.end_station_id), cbd.end_station_id, cbs.station_name from cleansed.bike_data as cbd
-join cleansed.bike_station as cbs ON cbd.end_station_id = cbs.station_id
+join cleansed.bike_station as cbs on cbd.end_station_id = cbs.station_id
 group by cbd.end_station_id, cbs.station_name
 order by count (cbd.end_station_id) asc
-limit 50) AS least_popular
-ORDER BY total_rides DESC;
+limit 50) as least_popular
+order by total_rides desc;
--- src/cleansed_to_modelled/start_stations_popularity.sql
+++ src/cleansed_to_modelled/start_stations_popularity.sql
@@ -1,16 +1,16 @@
 -- shows the 5 most and least popular start stations
 
-SELECT * FROM (
+select * from (
 select count (cbd.start_station_id) as total_rides, cbd.start_station_id, cbs.station_name from cleansed.bike_data as cbd
-join cleansed.bike_station as cbs ON cbd.start_station_id = cbs.station_id
+join cleansed.bike_station as cbs on cbd.start_station_id = cbs.station_id
 group by cbd.start_station_id, cbs.station_name
 order by count (cbd.start_station_id) desc
-limit 5) AS most_popular
-UNION
-SELECT * FROM (
+limit 5) as most_popular
+union
+select * from (
 select count (cbd.start_station_id), cbd.start_station_id, cbs.station_name from cleansed.bike_data as cbd
-join cleansed.bike_station as cbs ON cbd.start_station_id = cbs.station_id
+join cleansed.bike_station as cbs on cbd.start_station_id = cbs.station_id
 group by cbd.start_station_id, cbs.station_name
 order by count (cbd.start_station_id) asc
-limit 5) AS least_popular
-ORDER BY total_rides DESC;
+limit 5) as least_popular
+order by total_rides desc;
